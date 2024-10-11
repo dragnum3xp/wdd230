8bc4dcde0e11b2a1c9761b8aa8b627bb23479c8d
dragnum3xp <320235583@byui.edu>
2024-10-11 09:34:26 -0300

    mobile view updated

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -55,3 +55,26 @@ document.addEventListener("DOMContentLoaded", () => {
     saveLastVisitDate();
 });
 
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    
+    event.preventDefault();
+    const now = new Date();
+    
+    
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0'); 
+    const day = String(now.getDate()).padStart(2, '0');
+    
+   
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+
+    
+    const formattedTimestamp = `${year}/${month}/${day} ${hours}:${minutes}`;
+
+    
+    document.getElementById('timestamp').value = formattedTimestamp;
+
+    console.log('Timestamp:', document.getElementById('timestamp').value);
+});
